f887f73dccf7a5e1a28d53f07661472ad417d71e
sukycar <sukycar@gmail.com>
2019-11-16 18:53:14 +0100

    Initial Commit

---
+++ blicBuch/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  blicBuch
+//
+//  Created by Vladimir Sukanica on 11/16/19.
+//  Copyright © 2019 sukydeveloper. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
